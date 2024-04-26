12608d0042d32ccaaf19bac4b7cb8aa5bb33108e
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-04-26 14:11:00 -0700

    add some code changes

--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -19,7 +19,7 @@ const Education = () => {
                     University College of Engineering Anna University
                   </h4>
                   <h4 className="item-title">
-                    <i className="fa-solid fa-calendar"></i>
+                    <i className="fa-solid fa-calendar"></i>&nbsp;
                     2019 - 2023
                   </h4>
                 </div>
@@ -30,7 +30,7 @@ const Education = () => {
                     Bhagavan Higher Secondary School
                   </h4>
                   <h4 className="item-title">
-                    <i className="fa-solid fa-calendar"></i>
+                    <i className="fa-solid fa-calendar"></i>&nbsp;
                     2018 - 2019
                   </h4>
                 </div>
@@ -41,7 +41,7 @@ const Education = () => {
                     St Antony's Girls Higher Secondary School
                   </h4>
                   <h4 className="item-title">
-                    <i className="fa-solid fa-calendar"></i>
+                    <i className="fa-solid fa-calendar"></i>&nbsp;
                     2016 - 2017
                   </h4>
                 </div>
@@ -55,11 +55,27 @@ const Education = () => {
                   <h3 className="item-title">Android Development WorkShop</h3>
                   <h4 className="item-title">UCE-BIT Campus</h4>
                   <h4 className="item-title">
-                    <i className="fa-solid fa-calendar"></i>
+                    <i className="fa-solid fa-calendar"></i>&nbsp;
                     FEB - 2023
                   </h4>
                   <div className="education-btn">
                     <a href="" target="_blank">
+                      <button type="submit" className="submit-btn">
+                        View
+                      </button>
+                    </a>
+                  </div>
+                </div>
+                <div className="time-line-item">
+                  <div className="circle-dot"></div>
+                  <h3 className="item-title">Full Stack Development</h3>
+                  <h4 className="item-title">Guvi</h4>
+                  <h4 className="item-title">
+                    <i className="fa-solid fa-calendar"></i>&nbsp;
+                    SEP - 2023 to JAN - 2024
+                  </h4>
+                  <div className="education-btn">
+                    <a href="https://drive.google.com/file/d/19ELVA8tbkb_d_2vdvwb6oGcOTcQ-9X5s/view?usp=drive_link" target="_blank">
                       <button type="submit" className="submit-btn">
                         View
                       </button>
