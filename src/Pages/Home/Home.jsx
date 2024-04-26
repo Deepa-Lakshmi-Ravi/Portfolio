8ceee12cff109586824bb2287650b0e5ba2535a8
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-04-26 10:25:07 -0700

    changes in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { Fade, Zoom } from "react-awesome-reveal";
 import "./Home.css";
-import Profile from '../../assets/deepar.png';
+import Profile from '../../assets/Deepa.jpeg';
 
 const Home = () => {
   const [text] = useTypewriter({
