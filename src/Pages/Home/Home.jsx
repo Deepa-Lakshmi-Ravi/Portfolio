418dcf76d3d3be2b08465772ac10e161baadad30
Deepa-Lakshmi-Ravi <deviravichandren02@gmail.com>
2024-04-25 23:14:46 -0700

    change image

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -80,7 +80,7 @@ const Home = () => {
                     <Fade direction="500" cascade>
                       <div className="profile-photo">
                         <img
-                          src="/public/deepar.JPG"
+                          src="/src/image/deepar.JPG"
                           alt="profile picture"
                         ></img>
                       </div>
